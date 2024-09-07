f3c2feef93be744728b1bd9eb319580234f11d7e
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-07 04:57:52 +0300

    Map-MapGet-MapPost

--- Program.cs
+++ Program.cs
@@ -6,8 +6,25 @@ namespace ibrahimEzzat_C42_EngyoussefZakriaa_MVC02
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+            app.UseRouting();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Home", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from Home page!");
+                });
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Product", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from Product page!");
+                });
+            });
+            app.Run(async (HttpContext) => { await HttpContext.Response.WriteAsync("Your Requsted Page Not Found !!"); });
+            // app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }

4561d3b9aa5d2dee2819367a7479b744a4569a46
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-07 04:15:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace ibrahimEzzat_C42_EngyoussefZakriaa_MVC02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}

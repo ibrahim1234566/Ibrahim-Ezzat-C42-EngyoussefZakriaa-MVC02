fcfd1f2cd567e9186170ea6dd8ce8574a3ed2ba0
ibrahim1234566 <hemaazz123@gmail.com>
2024-09-07 05:45:06 +0300

    Parameters with Routing

--- Program.cs
+++ Program.cs
@@ -16,11 +16,40 @@ namespace ibrahimEzzat_C42_EngyoussefZakriaa_MVC02
                 });
             });
 
-            app.UseEndpoints(endpoints =>
+        /*    app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/Product", async context =>
                 {
                     await context.Response.WriteAsync("Hello from Product page!");
+                });
+            });*/
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Product/{ID :int?}", async context =>
+                {
+                    var id = context.Request.RouteValues["ID"];
+                    if(id != null)
+                    {
+                        Convert.ToInt32(id);
+                        await context.Response.WriteAsync($"Hello from Product page id => {id}");
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("Hello from Product page!");
+
+                    }
+                    
+                });
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Books/{id :int}/{Author}", async context =>
+                {
+                    int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+                    string name = context.Request.RouteValues["Author"].ToString();
+                    await context.Response.WriteAsync($"Hello from Books page id => {id} name=>{name}");
                 });
             });
             app.Run(async (HttpContext) => { await HttpContext.Response.WriteAsync("Your Requsted Page Not Found !!"); });
